68807f74fb257599fd764c3b356174b9d171656e
ahmetpala <ahmtpala13@gmail.com>
2025-03-16 15:32:54 +0100

    Cleanup with autopep8

--- paths.py
+++ paths.py
@@ -29,48 +29,61 @@ try:
         sys.path.append(setup_file["syspath"])
 
 except:
-    class SetupFileIsMissing(Exception): pass
-    raise SetupFileIsMissing('Please make a setpyenv.json file in the root directory.')
+    class SetupFileIsMissing(Exception):
+        pass
+    raise SetupFileIsMissing(
+        'Please make a setpyenv.json file in the root directory.')
+
 
 def load_yaml_config(path_configuration):
     try:
         with open(path_configuration, "r") as stream:
             try:
-                return(yaml.safe_load(stream))
+                return (yaml.safe_load(stream))
             except yaml.YAMLError as exc:
                 print(exc)
 
     except:
-        class SetupFileIsMissing(Exception):pass
-        raise SetupFileIsMissing('Please make a pipeline_config.yaml file in the root directory.')
+        class SetupFileIsMissing(Exception):
+            pass
+        raise SetupFileIsMissing(
+            'Please make a pipeline_config.yaml file in the root directory.')
+
 
 def path_to_echograms():
     # Directory path to echogram data
     return setup_file['path_to_echograms']
 
+
 def path_to_korona_data():
     # Directory path to predictions from the Korona algorithm in LSSS
     return setup_file['path_to_korona_data']
+
 
 def path_to_korona_transducer_depths():
     # Directory path to the transducer depths in the Korona predictions (necessary for vertical calibration)
     return setup_file['path_to_korona_transducer_depths']
 
+
 def path_to_trained_model():
     # Directory path to trained models
     return setup_file['path_to_trained_model']
+
 
 def path_to_zarr_files():
     # Directory path to zarr files
     return setup_file['path_to_zarr_files']
 
+
 def path_for_saving_figs():
     # Directory path for saving figures relating to results evaluation
     return setup_file['path_for_saving_figs']
 
+
 def path_for_saving_preds_labels():
     # Directory path for saving figures relating to results evaluation
     return setup_file['path_for_saving_preds_labels']
+
 
 def pipeline_config():
     return load_yaml_config(os.path.join(os.path.dirname(__file__), 'pipeline_config.yaml'))
